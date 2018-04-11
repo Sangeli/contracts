c144dbec41797067c0031e94f10cbca42ac6289f
Stephen Cefali <stephencefali@gmail.com>
2018-04-10 18:44:56 -0700

    fixes test config

--- solidity/truffle-config.js
+++ solidity/truffle-config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    networks: {
-        development: {
-        host: "localhost",
-        port: 8545,
-        network_id: "*", // Match any network id
-        gasPrice: 22000000000
-      }
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*', // Match any network id
+      gasPrice: 2200000000,
+      gas: 22000000
     }
-};
+  }
+}
